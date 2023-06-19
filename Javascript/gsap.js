dcd413cc6d7793fac7a59e14aaa36c4859604269
Ntoni596 <Ntoni596@gmail.com>
2023-06-20 09:33:01 +1200

    Tidying code and attempt to add in mapbox markers

--- Javascript/gsap.js
+++ Javascript/gsap.js
@@ -1,9 +1,14 @@
+/**
+ * Function to create fade-out animations.
+ */
 function fadeOut() {
+  // Move and fade out the button.
   TweenMax.to(".myBtn", 1, {
     y: -100,
     opacity: 0,
   });
 
+  // Move and fade out the screen after a delay.
   TweenMax.to(".screen", 2, {
     y: -400,
     opacity: 0,
@@ -11,33 +16,40 @@ function fadeOut() {
     delay: 2,
   });
 
-  // TweenMax.from('.overlay',2,{
+  // Uncomment the following if you want to animate the '.overlay' class.
+  // TweenMax.from('.overlay', 2, {
   //   ease: Power2.easeInOut
   // });
 
+  // Move the first overlay up after a delay.
   TweenMax.to(".overlay", 2, {
     delay: 2.6,
     top: "-150%",
     ease: Expo.easeInOut,
   });
 
+  // Move the second overlay up after a delay.
   TweenMax.to(".overlay-2", 2, {
     delay: 3,
     top: "-150%",
     ease: Expo.easeInOut,
   });
 
+  // Fade in the content after a delay.
   TweenMax.from(".content", 2, {
     delay: 3.2,
     opacity: 0,
     ease: Power2.easeInOut,
   });
 
+  // Move and fade in the content after a delay.
   TweenMax.to(".content", 2, {
     opacity: 1,
     y: "1vh",
     delay: 3.2,
     ease: Power2.easeInOut,
   });
+
+  // Update the z-index of the content to make it visible.
   document.getElementById("content").style.zIndex = "1";
 }
--- Javascript/vue-app.js
+++ Javascript/vue-app.js
@@ -1,7 +1,10 @@
+// Importing accommodation options from data.js
 import { accommodationOptions } from "./data.js";
 
 new Vue({
-  el: "#app",
+  el: "#app", // Mount Vue app on element with id "app"
+
+  // Define initial data
   data: {
     step: 1,
     people: 1,
@@ -12,19 +15,26 @@ new Vue({
     totalCost: null,
     error: "",
   },
+
+  // Compute derived values from data
   computed: {
+    // Get unique locations from accommodation options
     uniqueLocations() {
       return [
         ...new Set(accommodationOptions.map((option) => option.location)),
       ];
     },
+
+    // Filter accommodation options based on selected location
     filteredAccommodationOptions() {
       return accommodationOptions.filter(
         (option) => option.location === this.location
       );
     },
   },
+
   methods: {
+    // Transition effects for enter and leave
     enter(el, done) {
       TweenMax.fromTo(
         el,
@@ -41,15 +51,21 @@ new Vue({
         { opacity: 0, onComplete: done }
       );
     },
+
+    // Change step to next or previous
     nextStep() {
-      this.step++;
+      this.step += 1;
     },
     previousStep() {
-      this.step--;
+      this.step -= 1;
     },
+
+    // Select an accommodation option
     selectAccommodation(option) {
       this.selectedAccommodation = option;
     },
+
+    // Filter accommodations based on number of people and days
     getAccommodations() {
       return this.filteredAccommodationOptions.filter(
         (option) =>
@@ -59,10 +75,15 @@ new Vue({
           this.days <= option.maxDays
       );
     },
+
+    // Create a map with accommodation details
     createMap() {
+      // Set Mapbox access token
       mapboxgl.accessToken =
         "pk.eyJ1IjoiZGVlcGFram9zaGlueiIsImEiOiJjbGoyZXUzeTAwdjR4M25sbGFmY2pud3F0In0.IF95gpfqzUq2IL9g3YSfUQ";
-      var geojson = {
+
+      // Set GeoJSON for accommodation
+      const geojson = {
         type: "FeatureCollection",
         features: [
           {
@@ -75,13 +96,14 @@ new Vue({
               ],
             },
             properties: {
-              title: "Accomodation",
-              description: this.selectAccommodation.location,
+              title: this.selectedAccommodation.listingName,
+              description: this.selectedAccommodation.address,
             },
           },
         ],
       };
 
+      // Create new Mapbox map
       const map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
@@ -91,12 +113,46 @@ new Vue({
         ],
         zoom: 15,
       });
+
+      // Add accommodation to map on load
+      map.on("load", () => {
+        map.addSource("accommodation", {
+          type: "geojson",
+          data: geojson,
+        });
+
+        // Load marker image and add to map
+        map.loadImage(
+          "/img/mapbox-marker-icon-20px-green.png",
+          (error, image) => {
+            if (error) throw error;
+            map.addImage("custom-marker", image);
+            map.addLayer({
+              id: "points",
+              type: "symbol",
+              source: "accommodation",
+              layout: {
+                "icon-image": "custom-marker",
+                "icon-size": 1.5,
+                "text-field": "{title}",
+                "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+                "text-offset": [0, 0.6],
+                "text-anchor": "top",
+              },
+            });
+          }
+        );
+      });
     },
-    closeModal: function () {
+
+    // Close modal
+    closeModal() {
       console.log("Closing modal");
       this.selectedAccommodation = null; // Assuming this would close your modal
     },
   },
+
+  // Watch for changes in selected accommodation
   watch: {
     selectedAccommodation() {
       this.$nextTick(() => {
